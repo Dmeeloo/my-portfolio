1ee5fd56aeb1f17e87d72a344eebc7f8be6fa0a7
dmeeloo <gabrieldemelomeurer16@gmail.com>
2026-06-01 00:34:30 -0300

    First Commit

---
+++ src/components/sections/index.ts
@@ -0,0 +1,6 @@
+export { Hero } from "./Hero";
+export { About } from "./About";
+export { Experience } from "./Experience";
+export { Contact } from "./Contact";
+export { Projects } from "./Projects";
+export { Skills } from "./Skills";
