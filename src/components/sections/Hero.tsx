c5d98d9f81c16a7c52a68eb0d25acef2315754d3
dmeeloo <gabrieldemelomeurer16@gmail.com>
2026-06-01 23:41:26 -0300

    feat: iniciatedCss

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,13 +4,15 @@ import { motion } from "framer-motion"
 
 export function Hero() {
     return (
-        <section>
-            <motion.div initial={{ opacity: 0, y: 20}}
+        <section className="py-24 text-center bg-gray-950">
+            <motion.div initial={{ opacity: 0, y: -20}}
                         animate={{ opacity: 1, y: 0}}
-                        transition={{ duration: 0.5 }}>
-                <h1>Gabriel de Melo Meurer</h1>
-                <h2>Desenvolvedor Full-Stack</h2>
-                <p>Desenvolvedor Full Stack com 2 anos de experiência, trabalhando
+                        transition={{ duration: 1 }}>
+                <div className="flex item-center gap-100 justify-center">
+                    <h1 className="text-5xl font-bold text-white">Gabriel de Melo Meurer</h1>
+                    <h2 className="text-5xl font-bold text-white">Engenheiro de Software</h2>
+                </div>   
+                <p className="text-2xl text-gray-200">Engenheiro de Software com 2 anos de experiência, trabalhando
                 com React, Next.js, OracleDB, JavaScript, Python</p>
             </motion.div>
         </section>
